433bfb735bbdfc51d3360289f1ee8cb0650b1c99
yuchuga <yuan_chungschumi@hotmail.com>
2024-03-22 12:19:09 +0800

    remove default css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css';
 import Button from './components/Button';
 import Container from './components/Container';
 import Greet from './components/Greet';
@@ -8,9 +7,9 @@ import PersonList from './components/PersonList';
 import Status from './components/Status';
 import { nameList } from './constants';
 
-const buttonClick = (e: React.MouseEvent<HTMLButtonElement>, id: number) => {
-  console.log('Button Clicked', e, id)
-};
+// const buttonClick = (e: React.MouseEvent<HTMLButtonElement>, id: number) => {
+//   console.log('Button Clicked', e, id)
+// };
 
 function App() {
   return (
@@ -19,7 +18,7 @@ function App() {
       <PersonList names={nameList} />
       <Status status='loading' />
       <Heading>Placeholder</Heading>
-      <Button handleClick={buttonClick} />
+      <Button />
       <Input />
       <Container styles={{ border: '1px solid black', padding: '1rem' }} />
     </div>
